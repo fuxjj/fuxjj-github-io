5663e779abcc0aa7f4694a9cb517677cdea7adb3
Junaid Ahmed <108035524+fuxjj@users.noreply.github.com>
2023-06-15 21:46:04 +0100

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,11 @@
+function togglegcse(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function toggleAlevel(){
+    document.getElementById("popup-2").classList.toggle("active")
+}
+
+function toggleUniversity(){
+    document.getElementById("popup-3").classList.toggle("active")
+}
